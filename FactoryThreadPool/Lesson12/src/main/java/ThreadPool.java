10617db64b8fc4e9c3cf9ee09777b792e2f5fb46
Shipulina <61729850+preppy5116@users.noreply.github.com>
2024-02-14 11:09:06 +0300

    Delete FactoryThreadPool directory

--- FactoryThreadPool/Lesson12/src/main/java/ThreadPool.java
+++
@@ -1,9 +0,0 @@
-public interface ThreadPool {
-    void start();
-    void execute(Runnable runnable);
-
-    void interrupt();
-
-    int getThreadCount();
-    int getTaskCount();
-}
--- FactoryThreadPool/Lesson12/src/main/java/ThreadPoolType.java
+++
@@ -1,4 +0,0 @@
-public enum ThreadPoolType {
-    FIXED_THREAD_POOL,
-    SCALABLE_THREAD_POOL;
-}
